e8c449e0315071664fb8ad9c9c019e2a931a3e7d
Juliiis <jumartinnob@gmail.com>
2022-05-15 21:20:04 +0200

    function main added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const TicTacToe = require("./class");
+
 //here I'm going to write what I want to see
+function main(){
+    const game = new TicTacToe()
+    const resultArray = game.move(2,1)
+    console.log(toString(resultArray))
+}
+
+// this function is to show the grid in the correct way
+function toString(grid) {
+    var resultString = " "
+    for(let i=0; i <= grid.length; i++){
+        resultString += grid[i].toString(separator = ",") + "\n"
+    }
+    return resultString
+}
+
+main()
