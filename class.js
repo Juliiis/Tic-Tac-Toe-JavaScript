8b36ed107d8f79effea914f95a1b39faa21d2268
Juliiis <jumartinnob@gmail.com>
2022-05-15 21:19:19 +0200

    TicTacToe class added

---
+++ class.js
@@ -0,0 +1,12 @@
+// here I'm going to write the logic part of the code
+
+class TicTacToe {
+    constructor(){}
+    move(row, column) {
+        return [[(" ", " ", " ")], 
+                [(" ", " ", " ")], 
+                [(" ", "X", " ")]]
+    }
+}
+
+module.exports = TicTacToe;
